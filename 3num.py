ea201f1fc0faba115f49d1f7b1cb8ee0ec6630e4
Akanshthegoat <akanshkohli002@gmail.com>
2025-02-18 14:49:07 +0530

    Create 3num.py

---
+++ 3num.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))  
+b = int(input("Enter second number: "))  
+c = int(input("Enter third number: "))  
+
+if a == b:
+    print("Equal number:", a)
+elif a == c:
+    print("Equal number:", a)
+elif b == c:
+    print("Equal number:", b)
+else:
+    print("No two numbers are equal.")
